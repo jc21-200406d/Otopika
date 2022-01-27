ef859f626c2c36d817e8df5ba2ae540488815b84
200406 <200406@e0601-18.JC-21.local>
2022-01-27 12:29:52 +0900

    Otopikadai01を作成

---
+++ src/OtoKadai01.java
@@ -0,0 +1,33 @@
+import jp.jc21.t.yoshizawa.otopika.Otopika;
+
+/**
+ * 
+ */
+
+
+/**
+ * @author AB2B05
+ *
+ */
+public class OtoKadai01  {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Otopika oto = new Otopika();
+		oto.powerOn();
+		while(true) {
+			oto.lightAll();
+			oto.lightOff();
+			oto.lightGreen();
+			oto.lightGreenYellow();
+			oto.lightYellow();
+			oto.lightYellowRed();
+			oto.lightRed();
+			oto.lightGreenRed();
+		}
+
+	}
+
+}
